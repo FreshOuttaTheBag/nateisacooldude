1e3b2ede1a05f0ec2e38536f7aa4c393f7f97e7f
Corey Coleson <coreycoleson@gmail.com>
2021-01-21 02:32:16 -0500

    Added some stuff ;P

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const bp = require("body-parser");
+const app = express();
+const port = 3000;
+const path = require('path');
+
+app.use(express.static('public'));
+app.use(bp.json());
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/public/index.html')
+});
+
+app.listen(port, async function() {
+    console.log(`Example app listening at http://localhost:${port}`);
+});
